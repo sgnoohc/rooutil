73755df1abef2cf4fd82c697083e24caf0350099
Philip Chang <sgnoohc@gmail.com>
2017-11-01 12:24:06 -0700

    ported sections from plotmaker.cc to plotmaker.h

--- plotmaker.cc
+++ plotmaker.cc
@@ -1,45 +1,7 @@
 //  .
 // ..: P. Chang, philip@physics.ucsd.edu
 
-#ifndef plotmaker_cc
-#define plotmaker_cc
-
-#include "TCanvas.h"
-#include "TColor.h"
-#include "TPad.h"
-#include "TH2.h"
-#include "TH1.h"
-#include "TH1D.h"
-#include "TH1F.h"
-#include "THStack.h"
-#include "TList.h"
-#include "TCollection.h"
-#include "TString.h"
-#include "TLatex.h"
-#include "TMath.h"
-#include "TObjString.h"
-#include "TLegend.h"
-#include "TLegendEntry.h"
-#include "TROOT.h"
-#include "TSystem.h"
-#include "TStyle.h"
-#include "TGaxis.h"
-
-#include "RooStats/NumberCountingUtils.h"
-
-#include <algorithm>
-#include <cstdio>
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <map>
-#include <string>
-#include <utility>
-#include <cstdlib>
-#include <sstream>
-#include <iomanip>
-#include <string>
-#include <deque>
+#include "plotmaker.h"
 
 using namespace std;
 
@@ -1769,4 +1731,3 @@ std::vector<TH1*> plotmaker(
 //    }
 //}
 
-#endif
