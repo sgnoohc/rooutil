69e60514af56419b6ab3c93880bc5b5b78196649
Philip Chang <sgnoohc@gmail.com>
2017-11-01 12:23:31 -0700

    created plotmaker.h. it was long overdue

---
+++ plotmaker.h
@@ -0,0 +1,42 @@
+#ifndef plotmaker_h
+#define plotmaker_h
+
+#include "TCanvas.h"
+#include "TColor.h"
+#include "TPad.h"
+#include "TH2.h"
+#include "TH1.h"
+#include "TH1D.h"
+#include "TH1F.h"
+#include "THStack.h"
+#include "TList.h"
+#include "TCollection.h"
+#include "TString.h"
+#include "TLatex.h"
+#include "TMath.h"
+#include "TObjString.h"
+#include "TLegend.h"
+#include "TLegendEntry.h"
+#include "TROOT.h"
+#include "TSystem.h"
+#include "TStyle.h"
+#include "TGaxis.h"
+
+#include "RooStats/NumberCountingUtils.h"
+
+#include <algorithm>
+#include <cstdio>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <map>
+#include <string>
+#include <utility>
+#include <cstdlib>
+#include <sstream>
+#include <iomanip>
+#include <string>
+#include <deque>
+
+
+#endif
