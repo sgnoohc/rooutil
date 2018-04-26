86ed1315c0e3471da6c9549322ad2cf36fadbdf9
Philip Chang <sgnoohc@gmail.com>
2018-04-25 22:35:37 -0700

    update

---
+++ filter_func_calls.py
@@ -0,0 +1,50 @@
+#!/bin/env python
+
+import os
+import sys
+
+def help():
+    print "python {} SOURCECODE [CLASSHEADERCODE]".format(sys.argv[0])
+    sys.exit()
+
+fname = sys.argv[1]
+
+f = open(fname)
+lines = f.readlines()
+
+rmchar = ".!,;=<>&\"?|*+/-:"
+paran = "(){}[]"
+
+funcs = []
+for line in lines:
+    line = line.strip()
+    for c in rmchar:
+        line = line.replace(c, " ")
+    if line.find("()") != -1:
+        ls = line.split("()")
+        for item in ls[:-1]:
+            for p in paran:
+                item = item.replace(p, " ")
+            funcs.append(item.split()[-1])
+funcs = list(set(funcs))
+funcs.sort()
+
+if len(sys.argv) > 2:
+    cname = sys.argv[2]
+    c = open(cname)
+    lines = c.readlines()
+    funcnames = []
+    for line in lines:
+        line = line.strip()
+        if line.find("&") != -1 and line.find("const") != -1:
+            funcnames.append(line.split("&")[1].split("()")[0])
+    funcnames = list(set(funcnames))
+    funcnames.sort()
+
+    for f in funcnames:
+        if f in funcs:
+            print f
+else:
+    for f in funcs:
+        print f
+
