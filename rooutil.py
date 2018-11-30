4024b2d2f709ecd401c768016f8960e20cd4e384
Philip Chang <sgnoohc@gmail.com>
2018-11-29 19:45:43 -0800

    update

--- rooutil.py
+++ rooutil.py
@@ -6,13 +6,63 @@ import sys
 from errors import E
 
 #______________________________________________________________________________
-def get_histograms(list_of_file_names, hist_name):
+def frac_syst_hists(h, error, errordn=None):
+    # The error, and errordn can take many format
+
+    # If the error provided is a list
+    if isinstance(error, list):
+
+        # Checking various input
+        if len(error) != h.GetNbinsX():
+            raise ValueError("provided list of error list, but the list don't match the number of bins in the histogram.")
+        if errordn:
+            if not isinstance(errordn, list):
+                raise ValueError("provided list of errordn but the type of errordn is not list, while error is.")
+            if len(error) != len(errordn):
+                raise ValueError("provided list of errordn but the length do not match error.")
+
+        # loop over the fractional errors and set the histogram bin content based on it
+        hup = h.Clone()
+        for i in xrange(len(error)):
+            bc = hup.GetBinContent(i+1)
+            nb = bc + error[i] * bc
+            hup.SetBinContent(i+1, nb if nb > 0 else 1e-6)
+
+        # loop over the fractional errordn (or error if errordn wasn't provided) and set the histogram bin content based on it
+        hdn = h.Clone()
+        for i in xrange(len(errordn if errordn else error)):
+            bc = hdn.GetBinContent(i+1)
+            nb = bc - error[i] * bc
+            hdn.SetBinContent(i+1, nb if nb > 0 else 1e-6)
+
+        # Return systematic histograms
+        return [hup, hdn]
+
+#______________________________________________________________________________
+def apply_sf(h, sfs, file_name, hist_name):
+    labels = h.GetXaxis().GetLabels()
+    if labels:
+        h.GetXaxis().SetRange(1, h.GetXaxis().GetNbins())
+        h.GetXaxis().SetCanExtend(False)
+    for key in sfs:
+        if key in file_name:
+            for rptn in sfs[key]:
+                if rptn in hist_name:
+                    for i in xrange(0, h.GetNbinsX()+2):
+                        bc, be = h.GetBinContent(i), h.GetBinError(i)
+                        nb = E(bc, be) * E(sfs[key][rptn][0], sfs[key][rptn][1])
+                        h.SetBinContent(i, nb.val)
+                        #h.SetBinError(i, nb.err)
+
+#______________________________________________________________________________
+def get_histograms(list_of_file_names, hist_name, sfs={}):
     hists = []
     for file_name in list_of_file_names:
         f = r.TFile(file_name)
         try:
             h = f.Get(hist_name).Clone(hist_name)
             h.SetDirectory(0)
+            apply_sf(h, sfs, file_name, hist_name.split("_")[0])
             hists.append(h)
         except:
             print "Could not find", hist_name, "in", file_name
@@ -20,7 +70,7 @@ def get_histograms(list_of_file_names, hist_name):
     return hists
 
 #______________________________________________________________________________
-def get_list_of_histograms(list_of_file_names, hist_names):
+def get_list_of_histograms(list_of_file_names, hist_names, sfs={}):
     hists = []
     for file_name in list_of_file_names:
         f = r.TFile(file_name)
@@ -28,6 +78,7 @@ def get_list_of_histograms(list_of_file_names, hist_names):
             try:
                 h = f.Get(hist_name).Clone(hist_name)
                 h.SetDirectory(0)
+                apply_sf(h, sfs, file_name, hist_name.split("_")[0])
                 hists.append(h)
             except:
                 print "Could not find", hist_name, "in", file_name
@@ -36,7 +87,7 @@ def get_list_of_histograms(list_of_file_names, hist_names):
     return hists
 
 #______________________________________________________________________________
-def get_yield_histogram(list_of_file_names, regions, hsuffix="_cutflow"):
+def get_yield_histogram(list_of_file_names, regions, hsuffix="_cutflow", sfs={}):
     final_h = r.TH1F("yields", "", len(regions), 0, len(regions))
     yields = []
     for i in xrange(len(regions)):
@@ -47,6 +98,7 @@ def get_yield_histogram(list_of_file_names, regions, hsuffix="_cutflow"):
             try:
                 prefix = region.split("(")[0]
                 h = f.Get(prefix + hsuffix)
+                apply_sf(h, sfs, file_name, prefix)
                 binoffset = int(region.split("(")[1].split(")")[0]) if len(region.split("(")) > 1 else h.GetNbinsX()
                 bc = h.GetBinContent(binoffset)
                 be = h.GetBinError(binoffset)
@@ -61,15 +113,15 @@ def get_yield_histogram(list_of_file_names, regions, hsuffix="_cutflow"):
     return final_h
 
 #______________________________________________________________________________
-def get_summed_histogram(list_of_file_names, hist_names):
+def get_summed_histogram(list_of_file_names, hist_names, sfs={}):
     if isinstance(hist_names, list):
         hists = []
         #for hist_name in hist_names:
         #    hists.extend(get_histograms(list_of_file_names, hist_name))
-        hists.extend(get_list_of_histograms(list_of_file_names, hist_names))
+        hists.extend(get_list_of_histograms(list_of_file_names, hist_names, sfs))
         hist_name = hist_names[0] + "_plus_etc"
     else:
-        hists = get_histograms(list_of_file_names, hist_names)
+        hists = get_histograms(list_of_file_names, hist_names, sfs)
         hist_name = hist_names
     if len(hists) == 0:
         print "error no histograms are found query=", list_of_file_names, hist_names
@@ -241,6 +293,170 @@ def submit_metis(job_tag, samples_map, arguments_map="", exec_script="metis.sh",
         except KeyboardInterrupt:
             raw_input("Press Enter to force update, or Ctrl-C to quit.")
             print "Force updating..."
+
+#______________________________________________________________________________
+def write_shape_fit_datacard(sig=None, bgs=[], data=None, datacard_filename="datacard.txt", region_name="SR", hist_filename="hist.root", systs={}):
+
+    # Checking arguments
+    if not sig:
+        print "Error: No signal histogram provided for the statistics datacard writing."
+        return
+
+    if len(bgs) == 0:
+        print "Error: No background histograms provided for the statistics datacard writing."
+        return
+
+    if not data:
+        print "Warning: No data histogram provided for the statistics datacard writing."
+        print "data will be set to total bkg expectation. (of course rounded."
+        fakedata = bgs[0].Clone()
+        fakedata.Reset()
+        for b in bgs:
+            fakedata.Add(b)
+        for i in xrange(1,fakedata.GetNbinsX()+2):
+            b = fakedata.GetBinContent(i)
+            fakedata.SetBinContent(i, int(b) if b > 0 else 0)
+        data = fakedata
+
+
+    """
+    imax 1 number of bins
+    jmax * number of processes
+    kmax * number of nuisance parameters
+    ----------------------------------------------------------------------------------------------------------------------------------
+    shapes * * statinputs/hist_sm.root $PROCESS $PROCESS_$SYSTEMATIC
+    ----------------------------------------------------------------------------------------------------------------------------------
+    bin          SR
+    observation  48.0
+    ----------------------------------------------------------------------------------------------------------------------------------
+    bin                                     SR           SR           SR           SR           SR           SR           SR           SR
+    process                                 0            1            2            3            4            5            6            7
+    process                                 www          fake         photon       lostlep      qflip        prompt       ttw          vbsww
+    rate                                    11.691       7.186        2.654        31.690       1.654        1.190        3.470        7.837
+    ----------------------------------------------------------------------------------------------------------------------------------
+    JEC                     shape           1            -            1            -            -            1            1            1
+    LepSF                   shape           1            -            1            -            -            1            1            1
+    TrigSF                  shape           1            -            1            -            -            1            1            1
+    BTagHF                  shape           1            -            1            -            -            1            1            1
+    BTagLF                  shape           1            -            1            -            -            1            1            1
+    Pileup                  shape           1            -            1            -            -            1            1            1
+    FakeRateEl              shape           -            1            -            -            -            -            -            -
+    FakeRateMu              shape           -            1            -            -            -            -            -            -
+    FakeClosureEl           shape           -            1            -            -            -            -            -            -
+    FakeClosureMu           shape           -            1            -            -            -            -            -            -
+    LostLepSyst             shape           -            -            -            1            -            -            -            -
+    MjjModeling             shape           -            -            -            1            -            -            -            -
+    MllSSModeling           shape           -            -            -            1            -            -            -            -
+    Mll3lModeling           shape           -            -            -            1            -            -            -            -
+    SigXSec                 lnN             1.06         -            -            -            -            -            -            -
+    LumSyst                 lnN             1.025        -            1.025        -            1.025        1.025        1.025        1.025
+    vbsww_xsec              lnN             -            -            -            -            -            -            -            1.20
+    vbsww_validation        lnN             -            -            -            -            -            -            -            1.22
+    ttw_xsec                lnN             -            -            -            -            -            -            1.20         -
+    ttw_validation          lnN             -            -            -            -            -            -            1.18         -
+    photon_syst             lnN             -            -            1.50         -            -            -            -            -
+    qflip_syst              lnN             -            -            -            -            1.50         -            -            -
+    www_stat_in_ee          shape           1            -            -            -            -            -            -            -
+    www_stat_in_em          shape           1            -            -            -            -            -            -            -
+    www_stat_in_mm          shape           1            -            -            -            -            -            -            -
+    www_stat_out_ee         shape           1            -            -            -            -            -            -            -
+    """
+
+    # Check that the histograms have no zero or negative yields
+    for x in ([sig] + bgs):
+        for i in xrange(0, x.GetNbinsX()+2):
+            bc = x.GetBinContent(i)
+            if bc <= 0:
+                x.SetBinContent(i, 0)
+                x.SetBinError(i, 0)
+
+    # Processes that will be written out in each column
+    hists = [sig] + bgs
+    hists_names = [ x.GetTitle() for x in hists ]
+    hists_rates = [ x.Integral() for x in hists ]
+
+    # Create output file
+    f = open(datacard_filename, "w")
+
+    # Write the header
+    f.write("""imax 1 number of bins
+jmax * number of processes
+kmax * number of nuisance parameters
+----------------------------------------------------------------------------------------------------------------------------------
+shapes * * {} $PROCESS $PROCESS_$SYSTEMATIC
+----------------------------------------------------------------------------------------------------------------------------------
+""".format(hist_filename))
+
+    # Write the total observed data number
+    f.write("""bin          {}
+observation  {}
+----------------------------------------------------------------------------------------------------------------------------------
+""".format(region_name, data.Integral()))
+
+    # Write the column header with region name
+    f.write("""bin                                     {}
+""".format("".join(["{:13s}".format(region_name)]*(len(hists)))))
+
+    # Write the index of processes
+    f.write("""process                                 {}
+""".format("".join(["{:<13d}".format(i) for i in xrange(len(hists))])))
+
+    # Write the names of processes
+    f.write("""process                                 {}
+""".format("".join(["{:13s}".format(i) for i in hists_names]))) # TH1::SetTitle is set to the short name of a process not TH1::SetName
+
+    # Write the rates of processes
+    f.write("""rate                                    {}
+----------------------------------------------------------------------------------------------------------------------------------
+""".format("".join(["{:<13f}".format(i) for i in hists_rates])))
+
+    # Write the statistical uncertainties (the naming convention is {proc}_stat_{ibin})
+    for index, x in enumerate(hists):
+        for i in xrange(1, x.GetNbinsX() + 1):
+            f.write("""{:24s}shape           {}
+""".format(x.GetTitle()+"_stat_"+str(i), "".join(["{:13s}".format("1" if index == j else "-") for j in xrange(len(hists))])))
+
+    # Write the histograms to the hist file
+    tf = r.TFile(hist_filename, "recreate")
+    tf.cd()
+
+    # Write the nominal rate histograms
+    for h in hists:
+        h.SetName(h.GetTitle()) # The title was set to the short names of the process
+        h.SetTitle("rates histogram") # Just to give some description
+        h.Write()
+
+    # Write the statistcal uncertainty histograms
+    for h in hists:
+        for i in xrange(1, x.GetNbinsX() + 1):
+            for var in ["Up", "Down"]:
+                name = h.GetName() # TH1::SetName() is now the process name
+                stat_error_hist_name = name + "_" + name + "_stat_" + str(i) + var # The TH1 naming convention is defined in datacard.txt line of "shapes * * .... "
+                eh = h.Clone(stat_error_hist_name)
+                nc = eh.GetBinContent(i + 1) + eh.GetBinError(i + 1) if var == "Up" else eh.GetBinContent(i + 1) - eh.GetBinError(i + 1)
+                eh.SetBinContent(i + 1, nc if nc > 0 else 1e-6)
+                eh.Write()
+
+    # Treat the systematic histograms
+    for proc in systs:
+        for syst in systs[proc]:
+
+            # Write the line that declares the systematic
+            f.write("""{:24s}shape           {}
+""".format(proc + "_" + syst, "".join(["{:13s}".format("1" if proc == iproc else "-") for iproc in hists_names])))
+
+            # Then write the histograms to the output file
+            for index, h in enumerate(systs[proc][syst]): # There are two histograms to loop over and they are up and down variations
+                histname = proc + "_" + proc + "_" + syst + ("Up" if index == 0 else "Down")
+                h.SetName(histname)
+                h.SetTitle(syst)
+                h.Write()
+
+    # Write the data histogram
+    data.SetName("data_obs") # HiggsCombineTool wants data histograms to set to exactly this name
+    data.SetTitle("observed data")
+    data.Write()
+    tf.Close()
 
 if __name__ == "__main__":
     main()
