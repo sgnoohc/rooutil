6e7db678c3466e3f16abc1ef0ab76c3fc577c7cf
Philip Chang <sgnoohc@gmail.com>
2018-12-14 18:58:04 -0800

    update

--- module.h
+++ module.h
@@ -3,16 +3,29 @@
 
 #include "ttreex.h"
 
+#include <vector>
+#include <memory>
+
 namespace RooUtil
 {
     class Module
     {
-        private:
-            TTreeX* tx;
         public:
-            virtual ~Module() = 0;
+            TTreeX* tx;
+            void SetTTreeX(RooUtil::TTreeX*);
             virtual void AddOutput();
             virtual void FillOutput();
+    };
+
+    class Processor
+    {
+        public:
+            TTreeX* tx;
+            std::vector<unique_ptr<Module>> modules;
+            Processor(RooUtil::TTreeX*);
+            void AddModule(RooUtil::Module*);
+            void AddOutputs();
+            void FillOutputs();
     };
 }
 
