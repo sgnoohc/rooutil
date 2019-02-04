016a6fc2a0cd2847c5ca3626e9653a777587f897
Philip Chang <sgnoohc@gmail.com>
2019-02-04 06:25:48 -0800

    update

--- igprof.sh
+++ igprof.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-igprof -pp -d -z -o igprof.pp.gz ${1}
+igprof -pp -d -z -o igprof.pp.gz $*
 igprof-analyse --sqlite -d -v -g igprof.pp.gz | sqlite3 igprof.pp.sql3 >& /dev/null
 cp igprof.pp.sql3 ~/public_html/cgi-bin/data/
 echo "http://${HOSTNAME}/~${USER}/cgi-bin/igprof-navigator.py/igprof.pp/"
