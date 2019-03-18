33204db3313cc0ac0dd9f2bd31863339692e8c78
Philip Chang <sgnoohc@gmail.com>
2019-03-17 18:24:41 -0700

    update

--- addHistos.sh
+++ addHistos.sh
@@ -1,4 +1,5 @@
-# Samuel May <sjmay@ucsd.edu>
+# Originally written by Samuel May <sjmay@ucsd.edu>
+# Modified (slightly) by Philip Chang <philip@ucsd.edu>
 
 ### Function to add large numbers (>100) of histograms using hadd 		###
 ### hadd seems to have a limit on the number of histograms you can give it, 	###
@@ -7,26 +8,28 @@
 ### Arg1: name of output histogram (e.g. "histosMaster") 			###
 ### Arg2: name of input histogram (e.g. "histosToAdd_4.root" would be input 	###
 ### 	  as "histosToAdd"							###
-### Arg3: number of input histograms of the above form 				###
-### Arg4: (OPTIONAL) number of cores to run on (default 1) 			###
+### Arg3: (OPTIONAL) number of cores to run on (default 1) 			###
 function addHistos 
 {
-  if (( $# < 4 ))
+  if (( $# < 3 ))
   then
     nPar=1
   else
-    nPar=$4
+    nPar=$3
   fi
+
+  NFILES=$(ls ${2}*.root | wc -l)
+  echo $NFILES
 
   histosToAdd=""
   bigHistos=""
   idx1=1
   idx2=1
-  while (($idx1 <= $3))
+  while (($idx1 <= ${NFILES}))
   do
     for ((i=1; i<=100; i++))
     do
-      if (($idx1 <= $3))
+      if (($idx1 <= ${NFILES}))
       then
         if [ -e $2"_"$idx1".root" ]; then 
           histosToAdd=$histosToAdd" "$2"_"$idx1".root"
