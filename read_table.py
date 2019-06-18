7ad74e040527c186efc19dbb6d8e2fb405401b63
Philip Chang <sgnoohc@gmail.com>
2019-06-18 11:54:41 -0700

    table parser

--- read_table.py
+++ read_table.py
@@ -2,6 +2,7 @@
 #!/bin/env python
 
 import sys
+from  errors import E
 
 def read_table(fname):
 
@@ -9,13 +10,21 @@ def read_table(fname):
     
     lines = [ l.strip() for l in f.readlines() ]
     
+    categories = []
+    yields = {}
     for line in lines:
         if "Bin#" in line:
             line = "".join(["	"] + line.split()[3:])
             line = line.replace("|", "			")
-            print line
+            categories = line.split()
+            for category in categories:
+                yields[category] = []
         if "Bin" in line:
             line = "".join(["	"] + line.split()[3:])
             line = line.replace("|", "	")
-            line = line.replace(u"\u00B1".encode("utf-8"), "	" + u"\u00B1".encode("utf-8") + "	")
-            print line
+            line = line.replace(u"\u00B1".encode("utf-8"), ",")
+            for category, item in zip(categories, line.split()):
+                val, err = item.split(",")
+                yields[category].append(E(float(val), float(err)))
+
+    return yields
