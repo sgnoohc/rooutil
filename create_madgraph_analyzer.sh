13a777312d29605781ddf30bff7600dcc80774ab
Philip Chang <sgnoohc@gmail.com>
2020-02-01 06:07:49 -0800

    update

--- create_madgraph_analyzer.sh
+++ create_madgraph_analyzer.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-LHEGZFILE=$1
-gunzip $LHEGZFILE
-LHEFILENAME=${LHEGZFILE/.lhe.gz/.lhe} 
-ROOTFILENAME=${LHEGZFILE/.lhe.gz/.root}
+
+if [[ "$1" == *".gz"* ]]; then
+    LHEGZFILE=$1
+    gunzip $LHEGZFILE
+    LHEFILENAME=${LHEGZFILE/.lhe.gz/.lhe}
+else
+    LHEFILENAME=$1
+fi
+
+ROOTFILENAME=${LHEFILENAME/.lhe/.root}
 python ${DIR}/lhe2root.py ${LHEFILENAME} ${ROOTFILENAME}
 makeclass.sh -f -x $ROOTFILENAME Physics LHE tas lhe
 cp $DIR/process_madgraph.cc_template process.cc
--- lhe2root.py
+++ lhe2root.py
@@ -45,13 +45,17 @@ P_Z_v =r.vector('Double_t')()
 E_v =r.vector('Double_t')()
 M_v =r.vector('Double_t')()
 P4_v=r.vector('ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float>>')()
+Wgt_v =r.vector('Double_t')()
 
 # Create a struct which acts as the TBranch for non-vectors
 gROOT.ProcessLine( "struct MyStruct{ Int_t n_particles; };")
+gROOT.ProcessLine( "struct MyFloat{ Float_t weight; };")
 from ROOT import MyStruct
+from ROOT import MyFloat
 
 # Assign the variables to the struct
-s = MyStruct() 
+s = MyStruct()
+event_wgt = MyFloat()
 output_tree.Branch('n_particles',AddressOf(s,'n_particles'),'n_particles/I')
 output_tree.Branch("PID",PID_v)
 output_tree.Branch("P_X",P_X_v)
@@ -60,28 +64,52 @@ output_tree.Branch("P_Z",P_Z_v)
 output_tree.Branch("E",E_v)
 output_tree.Branch("M",M_v)
 output_tree.Branch("P4",P4_v)
+output_tree.Branch("Wgt",Wgt_v)
+output_tree.Branch('event_wgt',AddressOf(event_wgt,'weight'),'event_wgt/F')
 
 in_ev = 0 # To know when to look for particles we must know when we are inside an event
 in_ev_1 = 0 # The first line after <event> is information so we must skip that as well
+in_wgt = 0 # To know when to look for weights we must know when we are inside an event
+in_wgt_1 = 0 # The first line after <rwgt> is information so we must skip that as well
 s.n_particles = 0
 for line in input_file:
     line = line.lstrip()
     if line[:1] == "#":
         continue
-    
+
+    # First line after the <event>
     if in_ev_1 == 1:
+        event_wgt.weight = float(line.split()[2])
         in_ev_1 = 0
         in_ev = 1
         continue
-    
+
+    # First line after the <rwgt>
+    if in_wgt_1 == 1:
+        # <wgt id='rwgt_11'> +2.2580107e-03 </wgt>
+        # Check the weight
+        try:
+            wgt = line.rsplit('>')[1].split()[0]
+            Wgt_v.push_back( float(wgt) )
+            pass
+        except:
+            pass
+        in_wgt_1 = 0
+        in_wgt = 1
+        continue
+
     if line.startswith("<event>"):
         in_ev_1 = 1
         continue
-    
+
+    if line.startswith("<rwgt>"):
+        in_wgt_1 = 1
+        continue
+
     # Some versions of les houches have a pdf line that we don't care about here
     if line.startswith("#pdf"):
         continue
-    
+
     if in_ev == 1 and line.startswith("</event>"):
         output_tree.Fill()
         # Reset variables
@@ -93,9 +121,14 @@ for line in input_file:
         E_v.clear()
         M_v.clear()
         P4_v.clear()
+        Wgt_v.clear()
         in_ev = 0
         continue
-    
+
+    if in_wgt == 1 and line.startswith("</rwgt>"):
+        in_wgt = 0
+        continue
+
     if in_ev == 1:
         # Check the status of this particle
         try:
@@ -111,6 +144,16 @@ for line in input_file:
                 VecLep = Math.LorentzVector('ROOT::Math::PxPyPzE4D<float>')(float(line.split()[6]), float(line.split()[7]), float(line.split()[8]), float(line.split()[9]))
                 P4_v.push_back( VecLep )
                 pass
+            pass
+        except:
+            pass
+
+    if in_wgt == 1:
+        # <wgt id='rwgt_11'> +2.2580107e-03 </wgt>
+        # Check the weight
+        try:
+            wgt = line.rsplit('>')[1].split()[0]
+            Wgt_v.push_back( float(wgt) )
             pass
         except:
             pass
