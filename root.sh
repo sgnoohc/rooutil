fb7aacf283fc6a0186d965bef033dd9acc77cfce
Philip Chang <sgnoohc@gmail.com>
2020-04-12 20:19:24 -0700

    update

--- root.sh
+++ root.sh
@@ -2,20 +2,16 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-if $(type root &> /dev/null); then
-    :
-else
-    #echo "Setting up ROOT"
-    # export SCRAM_ARCH=slc6_amd64_gcc530   # or whatever scram_arch you need for your desired CMSSW release
-    export SCRAM_ARCH=slc6_amd64_gcc700   # or whatever scram_arch you need for your desired CMSSW release
-    export CMSSW_VERSION=CMSSW_9_2_0
-    # export SCRAM_ARCH=slc6_amd64_gcc630   # or whatever scram_arch you need for your desired CMSSW release
-    # export CMSSW_VERSION=CMSSW_10_1_0
-    source /cvmfs/cms.cern.ch/cmsset_default.sh
-    cd /cvmfs/cms.cern.ch/$SCRAM_ARCH/cms/cmssw/$CMSSW_VERSION/src
-    eval `scramv1 runtime -sh`
-    cd - > /dev/null
-fi
+#echo "Setting up ROOT"
+# export SCRAM_ARCH=slc6_amd64_gcc530   # or whatever scram_arch you need for your desired CMSSW release
+export SCRAM_ARCH=slc6_amd64_gcc700   # or whatever scram_arch you need for your desired CMSSW release
+export CMSSW_VERSION=CMSSW_9_2_0
+# export SCRAM_ARCH=slc6_amd64_gcc630   # or whatever scram_arch you need for your desired CMSSW release
+# export CMSSW_VERSION=CMSSW_10_1_0
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+cd /cvmfs/cms.cern.ch/$SCRAM_ARCH/cms/cmssw/$CMSSW_VERSION/src
+eval `scramv1 runtime -sh`
+cd - > /dev/null
 
 echo 'Setup following ROOT'
 which root
