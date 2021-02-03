4b53c28c20ce27365a96c4d3fef22ea8805bdaf1
Philip Chang <sgnoohc@gmail.com>
2021-02-02 19:28:18 -0800

    update to slc7

--- root.sh
+++ root.sh
@@ -3,11 +3,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 #echo "Setting up ROOT"
-# export SCRAM_ARCH=slc6_amd64_gcc530   # or whatever scram_arch you need for your desired CMSSW release
-export SCRAM_ARCH=slc6_amd64_gcc700   # or whatever scram_arch you need for your desired CMSSW release
-export CMSSW_VERSION=CMSSW_9_2_0
-# export SCRAM_ARCH=slc6_amd64_gcc630   # or whatever scram_arch you need for your desired CMSSW release
-# export CMSSW_VERSION=CMSSW_10_1_0
+export SCRAM_ARCH=slc7_amd64_gcc900   # or whatever scram_arch you need for your desired CMSSW release
+export CMSSW_VERSION=CMSSW_11_2_1
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 cd /cvmfs/cms.cern.ch/$SCRAM_ARCH/cms/cmssw/$CMSSW_VERSION/src
 eval `scramv1 runtime -sh`
