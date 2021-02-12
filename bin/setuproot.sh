cf84e112769c7de02ca6f121647967a34d273e49
Philip Chang <sgnoohc@gmail.com>
2021-02-12 08:39:54 -0800

    update setuproot

--- bin/setuproot.sh
+++ bin/setuproot.sh
@@ -2,8 +2,8 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-export SCRAM_ARCH=slc6_amd64_gcc700   # or whatever scram_arch you need for your desired CMSSW release
-export CMSSW_VERSION=CMSSW_9_2_0
+export SCRAM_ARCH=slc7_amd64_gcc700   # or whatever scram_arch you need for your desired CMSSW release
+export CMSSW_VERSION=CMSSW_10_0_0
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 cd /cvmfs/cms.cern.ch/$SCRAM_ARCH/cms/cmssw/$CMSSW_VERSION/src
 eval `scramv1 runtime -sh`
