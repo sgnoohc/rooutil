d4cf28697bdf22ea0ce76fcb40626afaa35782e8
Philip Chang <sgnoohc@gmail.com>
2021-03-16 15:58:56 -0700

    update

--- root.sh
+++ root.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 #echo "Setting up ROOT"
 export SCRAM_ARCH=slc7_amd64_gcc700   # or whatever scram_arch you need for your desired CMSSW release
-export CMSSW_VERSION=CMSSW_10_0_0
+export CMSSW_VERSION=CMSSW_10_2_13
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 cd /cvmfs/cms.cern.ch/$SCRAM_ARCH/cms/cmssw/$CMSSW_VERSION/src
 eval `scramv1 runtime -sh`
