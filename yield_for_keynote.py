09c5303316111dedd8a0994e922e0e370e72031a
Philip Chang <sgnoohc@gmail.com>
2021-03-18 17:33:10 -0700

    update

--- yield_for_keynote.py
+++ yield_for_keynote.py
@@ -18,13 +18,20 @@ lines = [ l.strip() for l in f.readlines() ]
 #         line = line.replace(u"\u00B1".encode("utf-8"), "	" + u"\u00B1".encode("utf-8") + "	")
 #         print line
 
+# delim=","
+delim=","
+
 for line in lines:
     if "Bin#" in line:
-        line = "".join(["	"] + line.split()[3:])
-        line = line.replace("|", ",,,")
+        line = "".join(["	"]  + ["binname"+delim] + line.split()[3:])
+        line = line.replace("|", "{}{}{}".format(delim, delim, delim))
         print line
-    if "Bin" in line:
-        line = "".join(["	"] + line.split()[3:])
-        line = line.replace("|", ",")
-        line = line.replace(u"\u00B1".encode("utf-8"), "," + u"\u00B1".encode("utf-8") + ",")
+    elif "|" in line:
+        if len(line.split()) > 1:
+            title = line.split()[1] + delim
+        else:
+            title = delim
+        line = "".join(["	"] + [title] + line.split()[3:])
+        line = line.replace("|", delim)
+        line = line.replace(u"\u00B1".encode("utf-8"), delim + u"\u00B1".encode("utf-8") + delim)
         print line[:-1]
